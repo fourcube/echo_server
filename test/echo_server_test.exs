4bb56e795b4e4f03d62fa6669cc2cf995ada7c94
Chris Grieger <chris@scolp.de>
2015-12-12 16:23:15 +0100

    Add unit test

--- test/echo_server_test.exs
+++ test/echo_server_test.exs
@@ -2,7 +2,10 @@ defmodule EchoServerTest do
   use ExUnit.Case
   doctest EchoServer
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "simply echoes everything back" do
+    {:ok, socket} = :gen_tcp.connect String.to_char_list("127.0.0.1"), 4040, [:binary, active: false]
+
+    :ok = :gen_tcp.send socket, "foo\n"
+    {:ok, "foo\n"} = :gen_tcp.recv(socket, 0)
   end
 end
