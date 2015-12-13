ddb17d05a979a35b11be6aa3fbdb0b5eccfe6bd4
Chris Grieger <chris@scolp.de>
2015-12-13 13:50:46 +0100

    Release 0.0.2

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule EchoServer.Mixfile do
 
   def project do
     [app: :echo_server,
-     version: "0.0.1",
+     version: "0.0.2",
      elixir: "~> 1.1",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
@@ -28,6 +28,6 @@ defmodule EchoServer.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [{:exrm, "1.0.0-rc7"}]
   end
 end
---
+++ rel/echo_server/lib/echo_server-0.0.2/ebin/echo_server.app
@@ -0,0 +1,7 @@
+{application,echo_server,
+             [{registered,[]},
+              {description,"echo_server"},
+              {applications,[kernel,stdlib,elixir,logger]},
+              {mod,{'Elixir.EchoServer',[]}},
+              {vsn,"0.0.2"},
+              {modules,['Elixir.EchoServer','Elixir.EchoServer.Impl']}]}.
